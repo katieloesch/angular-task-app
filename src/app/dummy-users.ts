58ad0175360bb3bf25e17046bf515ae21a75eed3
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-07 22:18:03 +0100

    dynamically add content to use user component

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Amaya Jiwe',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Sarah Lance',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Leonard Snart',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Mick Rory',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Ava Sharpe',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'John Constantine',
+    avatar: 'user-6.jpg',
+  },
+];
