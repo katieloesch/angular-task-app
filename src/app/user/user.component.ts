f5f75eeb236959517585fc19ad53309e35fa1ef5
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-11 19:27:46 +0100

    incorporate signal and computed into UserComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -11,14 +11,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imgPath = computed(
+    () => '../../assets/images/users/' + this.selectedUser().avatar
+  );
 
-  get imgPath() {
-    return '../../assets/images/users/' + this.selectedUser.avatar;
-  }
+  // get imgPath() {
+  //   return '../../assets/images/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
